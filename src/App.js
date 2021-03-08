d366aa52b3643b547828ebbd5594dde460f8fcff
Narayanan Perumbaillam <narayananperumba@gmail.com>
2021-03-08 15:39:26 +0530

    start of spacex app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Typography from '@material-ui/core/Typography';
+import Toolbar from '@material-ui/core/Toolbar';
+import ViewListIcon from '@material-ui/icons/ViewList';
+import ViewModuleIcon from '@material-ui/icons/ViewModule';
+import ToggleButton from '@material-ui/lab/ToggleButton';
+import ToggleButtonGroup from '@material-ui/lab/ToggleButtonGroup';
+import Button from '@material-ui/core/Button';
+
+const useStyles = makeStyles((theme) => ({
+  title: {
+    marginRight: theme.spacing(2),
+  }
+}));
 function App() {
+  const classes = useStyles();
+
+  const [view, setView] = React.useState('list');
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <AppBar position="sticky">
+        <Toolbar>
+          <Typography className={classes.title} variant="h6" noWrap>
+            SpaceX
+          </Typography>
+          <ToggleButtonGroup value={view} exclusive style={{ flex: 1 }}>
+            <ToggleButton title="List View" value="list" aria-label="list" >
+              <ViewListIcon style={{ color: "white" }} />
+            </ToggleButton>
+            <ToggleButton title="Grid View" value="module" aria-label="module">
+              <ViewModuleIcon style={{ color: "white" }} />
+            </ToggleButton>
+          </ToggleButtonGroup>
+          <Button color="inherit">Login</Button>
+          <Button color="inherit">Signup</Button>
+        </Toolbar>
+      </AppBar>
     </div>
   );
 }
